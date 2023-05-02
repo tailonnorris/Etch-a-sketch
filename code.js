5bf9df2a7c7b43fef7a69b957a599f9ea06b2989
tyler <thailer2000@gmail.com>
2023-05-01 21:16:36 -0400

    functionality added to buttons

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+let color = 'black'
+
 document.addEventListener("DOMContentLoaded",function(){
     createBoard(16);
     let buttonPopUp = document.getElementById('select-button')
@@ -17,6 +19,7 @@ document.addEventListener("DOMContentLoaded",function(){
  
      for (let i = 0; i < numDiv; i++) {
          let div = document.createElement('div')
+         div.addEventListener("mouseover", colorDiv)
          board.insertAdjacentElement("beforeend", div)
      }
  }
@@ -26,12 +29,32 @@ document.addEventListener("DOMContentLoaded",function(){
  
      let message = document.querySelector(".message")
  
-     if (typeof input !== "number"){
+     if  (isNaN(input)){
          message.innerHTML = "Please type a number"
      }
      else if (input < 1 || input > 100){
          message.innerHTML = "Please type a number between 1 and 100"
+         return input
      }
-     return input
+     
  }
+
+
+ function colorDiv(){
+    if (color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = 'black'
+    }
+ }
+
+ function setColor(colorChoice){
+     color = colorChoice
+ }
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
+}
  
